d406adbd0bda6d436980bc1218fe3afbf0932de9
gwojtyl <gwojtyl@gmail.com>
2020-02-05 20:59:24 +0100

    bledy 1.4

--- kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaSpringWebApplicationTests {
+public class KodillaSpringWebApplicationTests {
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
