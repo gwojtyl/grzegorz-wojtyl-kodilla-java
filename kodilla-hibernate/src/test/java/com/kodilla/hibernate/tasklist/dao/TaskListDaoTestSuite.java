6896e01503eb87cb6147a63d713b0f4fcfc4add0
gwojtyl <gwojtyl@gmail.com>
2020-02-06 17:54:27 +0100

    Moduł 13.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,54 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testTaskListDaoSave() {
+        //Given
+        TaskList taskList = new TaskList("toDo", "To do list");
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        int id = taskList.getId();
+        Optional<TaskList> readTaskList = taskListDao.findById(id);
+        Assert.assertTrue(readTaskList.isPresent());
+
+        //CleanUp
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("toDo", "To do list");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskLists = taskListDao.findByListName(listName);
+
+        //Then
+        Assert.assertEquals(1, readTaskLists.size());
+
+        //CleanUp
+        int id = readTaskLists.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
