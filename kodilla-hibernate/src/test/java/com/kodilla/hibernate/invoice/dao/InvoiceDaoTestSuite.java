00d7eaf271f1c0b210c614bfebbea673ab880dfa
gwojtyl <gwojtyl@gmail.com>
2020-02-07 15:36:29 +0100

    Moduł 13.3 poprawka

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -16,15 +16,16 @@ import java.math.BigDecimal;
 @SpringBootTest
 public class InvoiceDaoTestSuite {
     @Autowired
-    InvoiceDao invoiceDao;
+    private InvoiceDao invoiceDao;
     @Autowired
-    ProductDao productDao;
+    private ProductDao productDao;
+    @Autowired
+    private ItemDao itemDao;
 
     @Test
     public void testInvoiceDaoSave() {
         //Given
         Product product = new Product("wood");
-
         productDao.save(product);
         int productId = product.getId();
 
@@ -33,22 +34,35 @@ public class InvoiceDaoTestSuite {
         Item item3 = new Item(product, new BigDecimal(20), 10, new BigDecimal(200));
 
         Invoice invoice = new Invoice("2345SDF");
-
         invoice.getItems().add(item1);
         invoice.getItems().add(item2);
         invoice.getItems().add(item3);
+
+        invoiceDao.save(invoice);
+        int invoiceId = invoice.getId();
 
         item1.setInvoice(invoice);
         item2.setInvoice(invoice);
         item3.setInvoice(invoice);
 
-        invoiceDao.save(invoice);
-        int invoiceId = invoice.getId();
+        itemDao.save(item1);
+        int item1Id = item1.getId();
+        itemDao.save(item2);
+        int item2Id = item2.getId();
+        itemDao.save(item3);
+        int item3Id = item3.getId();
 
         //Then
         Assert.assertNotEquals(0, invoiceId);
+        Assert.assertNotEquals(0, productId);
+        Assert.assertNotEquals(0, item1Id);
+        Assert.assertNotEquals(0, item2Id);
+        Assert.assertNotEquals(0, item3Id);
 
         //CleanUp
+        itemDao.deleteById(item1Id);
+        itemDao.deleteById(item2Id);
+        itemDao.deleteById(item3Id);
         invoiceDao.deleteById(invoiceId);
         productDao.deleteById(productId);
     }
